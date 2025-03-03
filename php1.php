698d0c7d795f18b7774ff201bdb8fd423bd130a9
dzukov6 <144792875+dzukov6@users.noreply.github.com>
2025-03-03 11:16:00 +0200

    f

---
+++ php1.php
@@ -0,0 +1,15 @@
+<?php
+$nimi = "Daniel zukov";
+$sunniaasta = 2006;
+$tahtkuju = "kaljukits";
+
+echo "Minu nimi on $nimi.<br>";
+echo "Ma sündisin aastal $sunniaasta.<br>";
+echo "Minu tähtkuju on $tahtkuju.<br><br>";
+
+echo '"It\'s My Life" – Dr. Alban<br><br>';
+
+echo nl2br(" (\\(\\ \n");
+echo nl2br(" (-.-) \n");
+echo nl2br(" o_(\")(\") \n");
+?>
