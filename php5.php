b1e078899ef6058fca4d6b181c82872ad83ef91e
dzukov6 <144792875+dzukov6@users.noreply.github.com>
2025-09-15 12:45:25 +0300

    Update php5.php

--- php5.php
+++ php5.php
@@ -5,52 +5,45 @@ echo "<h3>Tüdrukute nimed:</h3>";
 foreach ($tydrukud as $nimi) {
     echo "$nimi<br>";
 }
-
 echo "<h3>Esimesed kolm nime:</h3>";
 echo implode(", ", array_slice($tydrukud, 0, 3));
-
 echo "<h3>Viimane nimi:</h3>";
 echo end($tydrukud);
-
 echo "<h3>Suvaline nimi:</h3>";
 echo $tydrukud[array_rand($tydrukud)];
 
-$autod = ["Subaru", "BMW", "Acura", "Mercedes-Benz", "Lexus", "Toyota", "Volkswagen", "GMC"];
+$autod = ["Subaru","BMW","Acura","Mercedes-Benz","Lexus","GMC","Volvo","Toyota","Volkswagen","Volkswagen","GMC","Jeep","Saab","Hyundai","Subaru","Mercedes-Benz",
+"Honda","Kia","Mercedes-Benz","Chevrolet","Chevrolet","Porsche","Buick","Dodge","GMC","Dodge","Nissan","Dodge","Jaguar","Ford","Honda","Toyota","Jeep",
+"Kia","Buick","Chevrolet","Subaru","Chevrolet","Chevrolet","Pontiac","Maybach","Chevrolet","Plymouth","Dodge","Nissan","Porsche","Nissan","Mercedes-Benz",
+"Suzuki","Nissan","Ford","Acura","Volkswagen","Lincoln","Mazda","BMW","Mercury","Mitsubishi","Ram","Audi","Kia","Pontiac","Toyota","Acura","Toyota","Toyota",
+"Chevrolet","Oldsmobile","Acura","Pontiac","Lexus","Chevrolet","Cadillac","GMC","Jeep","Audi","Acura","Acura","Honda","Dodge","Hummer","Chevrolet","BMW",
+"Honda","Lincoln","Hummer","Acura","Buick","BMW","Chevrolet","Cadillac","BMW","Pontiac","Audi","Hummer","Suzuki","Mitsubishi","Jeep","Buick","Ford"];
 $vin_koodid = ["1GKS1GKC8FR966658", "1FTEW1C87AK375821", "1G4GF5E30DF760067", "WAUGGAFC8CN433989"];
 
 echo "<h3>Autode arv:</h3>" . count($autod);
-
 echo "<h3>Kas massiivid on ühepikkused?</h3>" . (count($autod) == count($vin_koodid) ? "Jah" : "Ei");
-
 $toyota_arv = count(array_filter($autod, fn($mark) => $mark == "Toyota"));
 $audi_arv = count(array_filter($autod, fn($mark) => $mark == "Audi"));
 echo "<h3>Toyotade arv:</h3> $toyota_arv";
 echo "<h3>Audide arv:</h3> $audi_arv";
-
 $lyhikesed_vin = array_filter($vin_koodid, fn($vin) => strlen($vin) < 17);
 echo "<h3>Lühikesed VIN koodid:</h3>" . implode(", ", $lyhikesed_vin);
-
 $palgad = [1220, 1213, 1295, 1312, 1298, 1354, 1296, 1286, 1292, 1327, 1369, 1455];
 echo "<h3>2018. aasta keskmine palk:</h3>" . array_sum($palgad) / count($palgad);
-
 $firmad = ["Kimia", "Mynte", "Voomm", "Twiyo", "Layo", "Talane", "Gigashots", "Tagchat"];
 sort($firmad);
 echo "<h3>Firmad:</h3>" . implode(", ", $firmad);
-
 $riigid = ["Indonesia", "Canada", "Kyrgyzstan", "Germany", "Philippines", "Democratic Republic of the Congo"];
 usort($riigid, fn($a, $b) => strlen($b) - strlen($a));
 echo "<h3>Kõige pikem riigi nimi:</h3> " . $riigid[0] . " (" . strlen($riigid[0]) . " tähemärki)";
-
 $hiina_nimed = ["瀚聪", "月松", "雨萌", "展博", "雪丽", "哲恒", "慧妍", "博裕"];
 sort($hiina_nimed);
 echo "<h3>Esimene hiina nimi:</h3> " . $hiina_nimed[0];
 echo "<h3>Viimane hiina nimi:</h3> " . end($hiina_nimed);
-
 $google_nimed = ["Feake", "Bradwell", "Dreger", "Bloggett", "Lambole"];
 $otsitav = "Dreger";
 echo "<h3>Otsingutulemus:</h3>";
 echo in_array($otsitav, $google_nimed) ? "<div class='alert alert-success'>$otsitav leitud!</div>" : "<div class='alert alert-danger'>$otsitav ei leitud!</div>";
-
 $pildid = ["prentice.jpg", "freeland.jpg", "peterus.jpg", "devlin.jpg", "gabriel.jpg", "pete.jpg"];
 echo "<h3>Kolmas pilt:</h3> <img src='C:/xampp/htdocs/img/{$pildid[2]}' alt='Pilt'>";
 echo "<h3>Kõik pildid:</h3>";
@@ -58,3 +51,4 @@ foreach ($pildid as $pilt) {
     echo "<img src='C:/xampp/htdocs/img/$pilt' alt='Pilt'> ";
 }
 ?>
+
